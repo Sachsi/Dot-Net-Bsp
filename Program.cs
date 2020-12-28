6a3ff2c1847d787a627486b34da105b18eb7fefa
woodpeaker-design <sachsenklang@outlook.com>
2020-12-28 19:18:01 +0100

    created new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dot_Net_Bsp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
