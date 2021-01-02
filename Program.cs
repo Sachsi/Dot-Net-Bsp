3806028123094dbafbc8c315b8c67c86306ea431
woodpeaker-design <sachsenklang@outlook.com>
2021-01-02 22:22:32 +0100

    Debuggung example

--- Program.cs
+++ Program.cs
@@ -3,29 +3,41 @@ using Humanizer;
 
 namespace Dot_Net_Bsp
 {
+using System;
+using System.Diagnostics;
+
+namespace DotNetDebugging
+{
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Quantities");
-            HumanizeQutites();
-
-            Console.WriteLine("\nDate/Time Manipulation:");
-            HumanizeDates();
+            int result = Fibonacci(3);
+            Console.WriteLine("This message is readable by the end user.");
+            Trace.WriteLine("This is a trace message when tracing the app.");
+    	    Debug.WriteLine("This is a debug message just for developers.");
+            Console.WriteLine(result);
         }
+        static int Fibonacci(int n)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int sum = 0;
+            
+            Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+            Debug.WriteLine($"We are looking for the {n}th number");
 
-        static void HumanizeQutites()
-        {
-            Console.WriteLine("case".ToQuantity(0));
-            Console.WriteLine("case".ToQuantity(1));
-            Console.WriteLine("case".ToQuantity(5));
-        }
-        static void HumanizeDates()
-        {
-            Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
-            Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
-            Console.WriteLine(TimeSpan.FromDays(1).Humanize());
-            Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+            for (int i = 2; i <= n; i++)
+            {                  
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+                Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");    
+            }
+            // If n2 is 5 continue, else break.
+            Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
+            return n == 0 ? n1 : n2;
         }
     }
+}
 }
